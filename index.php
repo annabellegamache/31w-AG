ab6146c8ae43b3c919323ee909c9858370386a7a
Annabellle <annabellegamache@gmail.com>
2022-06-02 09:22:02 -0400

    debug dans le fichier index.php de mon thèeme

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <h1>Mon premier modèle Wordpress</h1>
 <?php
         if ( have_posts() ) :
-            while ( get_posts()) : the_post()?>
+            while ( have_posts()) : the_post()?>
             <?php the_title();?>
             <br>
         <?php endwhile;?>
