fdaf6843bf4e51c6e45b3664da9a4c81be4c2388
Annabellle <annabellegamache@gmail.com>
2022-06-06 17:07:35 -0400

    s4-c6-version de style.css est ajustée automatique avec filemtime()

--- index.php
+++ index.php
@@ -3,7 +3,12 @@
 <?php
         if ( have_posts() ) :
             while ( have_posts()) : the_post()?>
-             <p><?php the_title();?></p>
+             <h2><?php the_title();?></h2>
+             <p><?php the_content();?></p>
+             <?php
+                 $lien = '<a href="'.get_permalink().'"> lien </a>';
+             ?>
+            <?= wp_trim_words(get_the_content(), 20, $lien); ?>
         <?php endwhile;?>
     <?php endif;?>
 
