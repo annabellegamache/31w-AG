e0f2582122758b0921ffa25d2b506b1b0dcf7b12
Annabellle <annabellegamache@gmail.com>
2022-06-16 10:45:24 -0400

    ajout d'une boite modale dans le script js

--- short_2/script/message.js
+++ short_2/script/message.js
@@ -1,5 +1,14 @@
 (function(){
-    let elmOK = document.querySelector('#mon_bouton')
+
+    //boite modal
+    let elmboite__modal = document.createElement('div');
+    elmboite__modal.classList.add('boite__modal');
+    elmBody = document.querySelector('body');
+
+    elmBody.appendChild(elmboite__modal);
+
+    let elmOK = document.querySelector('#mon_bouton');
+
     elmOK.addEventListener('mousedown', function(){
         console.log('Bravo le script fonctionne');
     })
