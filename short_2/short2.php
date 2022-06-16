77b8b32f604582883d22efbb89896c69aa95027c
Annabellle <annabellegamache@gmail.com>
2022-06-16 10:27:07 -0400

    debug short2 extention maintenant ca fonctionne

--- short_2/short2.php
+++ short_2/short2.php
@@ -3,7 +3,7 @@
     *Plugin Name: short 2
     *Description: Un deuxième exemple de plugin utilisant shortcode en utilisant des dossiers pour la structure
     *Author: Annabelle Gamache
-    * Plugin URI: https://github.com/annabellegamache/31w-AG.git
+    *Plugin URI: https://github.com/annabellegamache/31w-AG.git
     */
 
 
@@ -13,13 +13,12 @@
         $version_js = filemtime(plugin_dir_path(__FILE__) . 'script/message.js' );
 
         wp_enqueue_style(   'sh2_31w_css', 
-                            plugin_dir_url(__FILE__, 
-                            '/style.css'), 
+                            plugin_dir_url(__FILE__) .'style.css', 
                             array(),
                             $version_css);
 
-        wp_enqueue_scripts(  'sh2_31w_js', 
-                            plugin_dir_url(__FILE__, '/script/message.js'), 
+        wp_enqueue_script(  'sh2_31w_js', 
+                            plugin_dir_url(__FILE__) . 'script/message.js', 
                             array(),  
                             $version_js, 
                             true);
@@ -32,7 +31,7 @@
     /////////////////////////////////////// HTML
     $contenu = '<code class="code__adresse">';
     $contenu .= "3 800, rue Sherbrooke Est Montréal (Québec) H1X 2A2 | 514 254-7131";
-    $contenu .= '</code>';
+    $contenu .= '</code>&nbsp; &nbsp;';
     $contenu .= '<button id="mon_bouton">OK</button>';
     
     return $contenu;
